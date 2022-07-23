cf648b61947e189f0142822b1932585430faef9a
Gechrist <gchris@hotmail.co.uk>
2022-07-23 13:14:35 +0300

    checkbox tests for mobile 6

--- pages/create/user.tsx
+++ pages/create/user.tsx
@@ -144,7 +144,7 @@ const NewUserProfile: FC = () => {
           <div className="flex flex-row bg-white mx-4 justify-center w-full border-b-2 border-gray-400 space-x-2">
             <label htmlFor="admin">Administrator:</label>
             <input
-              className="mb-1.5"
+              // className="mb-2"
               type="checkbox"
               id="admin"
               {...register('isAdmin')}
