e9a037f286b3fd304863ccd391405fef899095f7
Gechrist <gchris@hotmail.co.uk>
2022-07-23 11:45:43 +0300

    no h-screen w-screen test

--- components/indexCarSection.tsx
+++ components/indexCarSection.tsx
@@ -1,40 +1,41 @@
 import { FC } from 'react';
-// import Image from 'next/image';
+import Image from 'next/image';
 import formatter from '../utils/prices';
-// import Link from 'next/link';
+import Link from 'next/link';
 import { IndexCarModel } from '../pages/index';
-// import { useContext } from 'react';
-// import { StateContext } from '../utils/context';
+import { useContext } from 'react';
+import { StateContext } from '../utils/context';
 
 type Props = {
   car: IndexCarModel;
 };
 
 const IndexCarSection: FC<Props> = ({ car }: Props) => {
-  // const { dispatch } = useContext<any>(StateContext);
+  const { dispatch } = useContext<any>(StateContext);
 
-  // const AddtoCart = (
-  //   _id: string,
-  //   model: string,
-  //   quantity: number,
-  //   price: number
-  // ) => {
-  //   dispatch({
-  //     type: 'ADDTO_CART',
-  //     payload: { _id, model, quantity, price },
-  //   });
-  // };
+  const AddtoCart = (
+    _id: string,
+    model: string,
+    quantity: number,
+    price: number
+  ) => {
+    dispatch({
+      type: 'ADDTO_CART',
+      payload: { _id, model, quantity, price },
+    });
+  };
 
+  // min-h-screen min-w-screen
   return (
-    <div className=" grid items-center">
+    <div className="grid items-center">
       <div className="flex-col md:flex-row md:h-auto flex">
         <div className="flex order-last pt-2 flex-col space-y-4 md:space-y-8 w-full md:w-3/6">
-          {/* <Link href={`/cars/${car._id}`} passHref>
+          <Link href={`/cars/${car._id}`} passHref>
             <h1 className="text-center link">{car.model}</h1>
-          </Link> */}
+          </Link>
           <p className="text-center italic p-1">{car.description}</p>
           <p className="text-center">{formatter.format(car.price)}</p>
-          {/* {car.quantity > 0 ? (
+          {car.quantity > 0 ? (
             <div className="flex justify-center">
               <button
                 onClick={() => AddtoCart(car._id, car.model, 1, car.price)}
@@ -45,10 +46,10 @@ const IndexCarSection: FC<Props> = ({ car }: Props) => {
             </div>
           ) : (
             <p className="text-center">Out of stock</p>
-          )} */}
+          )}
         </div>
         <div className="flex flex-col w-full md:w-3/6">
-          {/* {car.featuredImage && (
+          {car.featuredImage && (
             <div className="relative block md:justify-end w-full h-72 md:h-full">
               <div className="absolute border-0 w-full h-2/6 md:h-1/6 z-50 top-0 bg-gradient-to-b from-gray-900" />
               <div className="absolute border-0 w-full h-2/6 md:h-1/6 z-50 bottom-0 bg-gradient-to-t from-gray-900" />
@@ -60,7 +61,7 @@ const IndexCarSection: FC<Props> = ({ car }: Props) => {
                 objectFit="cover"
               />
             </div>
-          )} */}
+          )}
         </div>
       </div>
     </div>
