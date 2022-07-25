15a11f056cebc895696e749df076aecfb54bc21f
Gechrist <gchris@hotmail.co.uk>
2022-07-25 12:56:54 +0300

    landing page mobile fix

--- components/mainLandingPage.tsx
+++ components/mainLandingPage.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-// import Image from 'next/image';
-// import MainImage from '../public/images/index.jpg';
 
-const mainLandingPage = () => {
+const MainLandingPage = () => {
   return (
     <div
       data-menuanchor="top"
       className="section bg-[url('../public/images/index.jpg')] bg-cover bg-center"
     >
-      {/* <div className="top-0 z-0 block">
-        <Image
-          src={MainImage}
-          alt="main background image"
-          aria-label="main background image"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div> */}
       <div className="absolute bottom-14 left-0 right-0 flex justify-center">
         <div className="h-12 w-42 px-2 rounded-2xl border-2 border-white absolute">
           <div className="h-4 w-1 animate-bounce block mt-2 mx-auto bg-white relative border-2 border-white rounded-lg"></div>
@@ -27,4 +16,4 @@ const mainLandingPage = () => {
   );
 };
 
-export default mainLandingPage;
+export default MainLandingPage;
