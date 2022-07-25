a18b58939cc546f69bb2e515cc6c9a8829f613d7
Gechrist <gchris@hotmail.co.uk>
2022-07-25 11:52:27 +0300

    landing page test

--- components/mainLandingPage.tsx
+++ components/mainLandingPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import Image from 'next/image';
-import MainImage from '../public/images/index.jpg';
+// import Image from 'next/image';
+// import MainImage from '../public/images/index.jpg';
 
 const mainLandingPage = () => {
   return (
-    <div data-menuanchor="top" className="section">
-      <div className="top-0 z-0 block">
+    <div
+      data-menuanchor="top"
+      className="section bg-[url('../public/images/index.jpg')] bg-cover"
+    >
+      {/* <div className="top-0 z-0 block">
         <Image
           src={MainImage}
           alt="main background image"
@@ -13,7 +16,7 @@ const mainLandingPage = () => {
           layout="fill"
           objectFit="cover"
         />
-      </div>
+      </div> */}
       <div className="absolute bottom-14 left-0 right-0 flex justify-center">
         <div className="h-12 w-42 px-2 rounded-2xl border-2 border-white absolute">
           <div className="h-4 w-1 animate-bounce block mt-2 mx-auto bg-white relative border-2 border-white rounded-lg"></div>
